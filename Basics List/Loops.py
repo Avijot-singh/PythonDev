df6b6a385ceee29d79e0964da697988262f03fdf
Avijot Singh <Singhavijot17@gmail.com>
2024-05-14 19:27:28 +1000

    Loops.py

---
+++ Basics List/Loops.py
@@ -0,0 +1,26 @@
+# For Loops
+"""
+- A for loop is used to iterate ovr a sequence ( such as a list, tuple , or string)
+- Use for loop when you know exactly how many times you want to execute the loop 
+
+Example : 
+colours = ['blue', 'red', black, 'orange']
+for i in colours
+print(i)
+"""
+
+
+
+
+
+#While Loop
+"""
+- A while loop is used to repeatedly execute a block of code as long as a certain condition is true
+- Use while loop when you don't know how many times you  need to execute the loop before hand
+
+Example:
+i = 1 
+while i < 5:
+    Print(i)
+    i += 1
+"""

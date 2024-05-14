c06ec51d71719c21ed0a29b749414ba656667eb5
Avijot Singh <Singhavijot17@gmail.com>
2024-05-14 18:53:38 +1000

    New Learnings

---
+++ Basics List/Operators.py
@@ -0,0 +1,40 @@
+#Operators
+"""
+x = 2
+y = 3
+x += 2 # Adding a value of 2 in x
+y *= 2 # Multiplying 2 with the value of y
+z = x + y
+
+print(z)
+"""
+# Unirary Operator
+"""
+n = 7
+n = -7 # Makes the value negative so it will print -7
+print(n)
+"""
+# Relational Operator
+"""
+a = 100
+b = 50 
+
+c = a > b
+d = a < b 
+e = a == b
+f = a <= b
+g = a >= b
+h = a != b
+
+print(h)
+"""
+# Logical Operators (AND OR NOT)
+a = 5
+b = 4
+c = a < 8 and b < 5 # and means that both the statements should be true
+d = a < 8 and b < 2 
+
+e = a == 5 or b < 5 # Or means that one or another condition are true 
+
+f = a != 5 # not means that it does not equal to 
+print(e)
